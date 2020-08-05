51b7da35df5a66b073267f41bfd021d200448a5c
Renier Trenuela <rtrenuela@objectbright.com>
2020-08-05 12:40:55 +0800

    add loader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,17 +19,72 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <!-- CSS Files -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
     <link href="/template/assets/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="/template/assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet"/>
     <link href="/css/main.css" rel="stylesheet"/>
     <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet"/>
 
     @yield('css')
+    <style>
+        /* Center the loader */
+        #loader {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            z-index: 1;
+            width: 150px;
+            height: 150px;
+            margin: -75px 0 0 -75px;
+            border: 16px solid #f3f3f3;
+            border-radius: 50%;
+            border-top: 16px solid #3498db;
+            width: 120px;
+            height: 120px;
+            -webkit-animation: spin 2s linear infinite;
+            animation: spin 2s linear infinite;
+        }
+
+        @-webkit-keyframes spin {
+            0% { -webkit-transform: rotate(0deg); }
+            100% { -webkit-transform: rotate(360deg); }
+        }
+
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+        }
+
+        /* Add animation to "page content" */
+        .animate-bottom {
+            position: relative;
+            -webkit-animation-name: animatebottom;
+            -webkit-animation-duration: 1s;
+            animation-name: animatebottom;
+            animation-duration: 1s
+        }
+
+        @-webkit-keyframes animatebottom {
+            from { bottom:-100px; opacity:0 }
+            to { bottom:0px; opacity:1 }
+        }
+
+        @keyframes animatebottom {
+            from{ bottom:-100px; opacity:0 }
+            to{ bottom:0; opacity:1 }
+        }
+
+        #myDiv {
+            display: none;
+            text-align: center;
+        }
+    </style>
 </head>
 
 @isset($page_name)
-    <body class="">
-    <div class="wrapper">
+    <body onload="myFunction()" style="margin:0;">
+    <div id="loader"></div>
+    <div class="wrapper animate-bottom" style="display:none;" id="myDiv">
         @include('layouts.partials.sidebar')
 
         <div class="main-panel" id="main-panel">
@@ -60,6 +115,16 @@
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
+        var myVar;
+
+        function myFunction() {
+            myVar = setTimeout(showPage, 3000);
+        }
+
+        function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+        }
     </script>
     @yield('scripts')
     </body>
