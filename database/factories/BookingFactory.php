02f7777badb304778a205a4691da6dfe5d1efe58
Renier Trenuela <rtrenuela@objectbright.com>
2020-08-07 16:23:25 +0800

    DB for settings

---
+++ database/factories/BookingFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Booking;
+use Faker\Generator as Faker;
+
+$factory->define(Booking::class, function (Faker $faker) {
+    return [
+        'customer_id' => 3,
+        'vehicle'     => $faker->randomElement(['motorcycle', 'car']),
+        'service'     => $faker->randomElement(['pabili', 'padala', 'pa-grocery']),
+        'status'      => 'pending',
+        'schedule'    => $faker->dateTime,
+        'pick-up'     => $faker->address,
+        'drop-off'    => $faker->address,
+        'amount'      => 300,
+    ];
+});
