3855807e34a6241aaddef3cbbbcdb4e8d1bbb393
Jerome Sarmiento <jsarmiento@objectbright.com>
2020-10-02 22:35:51 +0800

    Enhance and Modify Delivery Controller

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -10,11 +10,22 @@ use App\Transformers\DeliveryFetchTransformer;
 
 class DeliveryController extends Controller
 {
+    /**
+     * Inital page for deliver.
+     *
+     * @return mixed
+     */
     public function index()
     {
         return view('delivery', ['page_name' => 'Delivery']);
     }
 
+    /**
+     * Gets data for Booking.
+     *
+     * @param Booking $booking
+     * @return array
+     */
     public function fetch(Booking $booking)
     {
         return [
@@ -24,32 +35,40 @@ class DeliveryController extends Controller
         ];
     }
 
+    /**
+     * Reserved a booking.
+     *
+     * @param $id
+     * @return mixed
+     */
     public function mine($id)
     {
         $pubnub = new PubNubConnect();
         $pubnub->message("Reserved!");
 
-        Booking::query()
-               ->where('id', $id)
-               ->update([
-                   'status'   => 'accepted',
-                   'rider_id' => auth()->id(),
-               ]);
+        Booking::query()->where('id', $id)->update([
+                'status'   => 'accepted',
+                'rider_id' => auth()->id(),
+            ]);
 
         return redirect()->back()->with('success', 'Request is yours now!');
     }
 
+    /***
+     * Completes a Booking.
+     * 
+     * @param $id
+     * @return mixed
+     */
     public function complete($id)
     {
         $pubnub = new PubNubConnect();
         $pubnub->message("Delivered!");
 
-        Booking::query()
-               ->where('id', $id)
-               ->update([
-                   'status'   => 'complete',
-                   'rider_id' => auth()->id(),
-               ]);
+        Booking::query()->where('id', $id)->update([
+                'status'   => 'complete',
+                'rider_id' => auth()->id(),
+            ]);
 
         return redirect()->back()->with('success', 'Request is yours now!');
     }
