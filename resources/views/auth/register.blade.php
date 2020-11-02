1287e87060133406095c0294c1b3e858a92c94ad
MJCastilloII <castilloiimaryjoy@gmail.com>
2020-11-02 12:08:33 -0500

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,39 +16,39 @@
                             <div class="row justify-content-center">
                                 <div class="col-md-6">
                                     <label>Full Name</label>
-                                    <input type="text" name="name" class="form-control">
+                                    <input type="text" name="name" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>E-mail</label>
-                                    <input type="text" name="email" class="form-control">
+                                    <input type="text" name="email" class="form-control"  style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Contact No.</label>
-                                    <input type="text" name="contact" class="form-control">
+                                    <input type="text" name="contact" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Birth Date</label>
-                                    <input type="date" name="birth_date" class="form-control">
+                                    <input type="date" name="birth_date" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-12">
                                     <label>Address</label>
-                                    <input type="text" name="address" class="form-control">
+                                    <input type="text" name="address" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Country</label>
-                                    <input type="text" name="country" class="form-control">
+                                    <input type="text" name="country" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Postal Code</label>
-                                    <input type="text" name="postal_code" class="form-control">
+                                    <input type="text" name="postal_code" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Password</label>
-                                    <input type="password" name="password" class="form-control">
+                                    <input type="password" name="password" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Confirm Password</label>
-                                    <input type="password" name="password_confirmation" class="form-control">
+                                    <input type="password" name="password_confirmation" class="form-control" style="background-color: white; border-color: black;">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Profile Picture</label>
@@ -68,7 +68,7 @@
                             </div>
                             <div class="row justify-content-center">
                                 <div class="col-md-3">
-                                    <button class="btn btn-round btn-success">Register now</button>
+                                    <button class="btn btn-round btn-success" style="background-color: var(--danger);">Register now</button>
                                 </div>
                             </div>
                         </form>
