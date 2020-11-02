bfbda9bad6bb99aadaa980cbeeedd2d848346b03
MJCastilloII <castilloiimaryjoy@gmail.com>
2020-11-02 14:18:05 -0500

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,8 +8,8 @@
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-md-8">
-               <!--  <div class="card login-form">
-                    <div class="card-header">{{ __('Registration') }}</div> -->
+                <div class="card login-form">
+                    <div class="card-header">{{ __('Registration') }}</div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                             @csrf
