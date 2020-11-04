9ddc5ae62b91c51ac45edf46af0d7935b64c3ec4
Renier Trenuela <rtrenuela@objectbright.com>
2020-11-04 13:13:26 +0800

    New implementation

--- resources/views/layouts/app-map.blade.php
+++ resources/views/layouts/app-map.blade.php
@@ -8,100 +8,32 @@
           name='viewport'/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <title>
-        BEX
+        {{ env('APP_NAME') }}
     </title>
 
-    <!-- CSS only -->
+    <!-- CDN Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!-- CDN LEAFLET CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.3/leaflet.css" rel="stylesheet" type="text/css"/>
+
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <!-- CSS Files -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
-    <link href="/template/assets/css/bootstrap.min.css" rel="stylesheet"/>
-    <link href="/css/main.css" rel="stylesheet"/>
     @yield('css')
     <style>
-        /* Center the loader */
-        #loader {
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            z-index: 1;
-            width: 150px;
-            height: 150px;
-            margin: -75px 0 0 -75px;
-            border: 16px solid #f3f3f3;
-            border-radius: 50%;
-            border-top: 16px solid #3498db;
-            width: 120px;
-            height: 120px;
-            -webkit-animation: spin 2s linear infinite;
-            animation: spin 2s linear infinite;
-        }
-
-        @-webkit-keyframes spin {
-            0% {
-                -webkit-transform: rotate(0deg);
-            }
-            100% {
-                -webkit-transform: rotate(360deg);
-            }
-        }
-
-        @keyframes spin {
-            0% {
-                transform: rotate(0deg);
-            }
-            100% {
-                transform: rotate(360deg);
-            }
-        }
-
-        /* Add animation to "page content" */
-        .animate-bottom {
-            position: relative;
-            -webkit-animation-name: animatebottom;
-            -webkit-animation-duration: 1s;
-            animation-name: animatebottom;
-            animation-duration: 1s
-        }
-
-        @-webkit-keyframes animatebottom {
-            from {
-                bottom: -100px;
-                opacity: 0
-            }
-            to {
-                bottom: 0px;
-                opacity: 1
-            }
-        }
-
-        @keyframes animatebottom {
-            from {
-                bottom: -100px;
-                opacity: 0
-            }
-            to {
-                bottom: 0;
-                opacity: 1
-            }
-        }
-
-        #myDiv {
-            display: none;
-            /*text-align: center;*/
+        .btn {
+            border-radius: 0 !important;
         }
     </style>
 </head>
-<body onload="myFunction()" style="margin:0;">
-<div id="loader"></div>
+<body>
 @yield('content')
 <!--   Core JS Files   -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/map.js') }}"></script>
 <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
 @include('layouts.partials.notification')
 <script>
@@ -110,17 +42,12 @@
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-    var myVar;
-
-    function myFunction() {
-        myVar = setTimeout(showPage, 1000);
-    }
-
     function showPage() {
         document.getElementById("map").style.display = "none";
         document.getElementById("map").style.display = "block";
     }
 </script>
+
 @yield('scripts')
 </body>
 
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -13,65 +13,80 @@
 @endsection
 
 @section('scripts')
-    <!--suppress ALL -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-          crossorigin=""/>
-    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-            crossorigin=""></script>
+    <!-- leaflet library -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.3/leaflet.js"></script>
+    <script type="text/javascript" src="https://tiles.unwiredmaps.com/js/leaflet-unwired.js"></script>
 
-    <!-- CSS and JS files for Search Box -->
-    <script src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet/0.0.1-beta.5/esri-leaflet.js"></script>
+    <!-- location control -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol/dist/L.Control.Locate.min.css"/>
+    <!-- location control -->
+    <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol/dist/L.Control.Locate.min.js"
+            charset="utf-8"></script>
+    <!-- geocoding plugin -->
+    <link rel="stylesheet"
+          href="https://maps.locationiq.com/v2/libs/leaflet-geocoder/1.9.5/leaflet-geocoder-locationiq.min.css">
     <script
-        src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.js"></script>
-    <link rel="stylesheet" type="text/css"
-          href="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.css">
+        src="https://maps.locationiq.com/v2/libs/leaflet-geocoder/1.9.5/leaflet-geocoder-locationiq.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-plugins/3.0.2/control/Permalink.js"></script>
+
     <script type="text/javascript">
-        // This setup the leafmap object by linking the map() method to the map id (in <div> html element)
+        // Maps access token goes here
+        var key = 'pk.d77cc8e3aa8354c6533770b02a1c9046';
+
+        // Add layers that we need to the map
+        var streets = L.tileLayer.Unwired({key: key, scheme: "streets"});
+
+        // Initialize the map
         var map = L.map('map', {
-            center: [14.164862797000069, 120.8616300000001],
-            zoom: 5,
-            // minZoom: 1.5,
-            //  maxZoom: 1.5
+            center: [12.2205183, 122.3861085], // Map loads with this location as center
+            zoom: 6,
+            scrollWheelZoom: true,
+            layers: [streets],
+            zoomControl: false
         });
 
-        // Start adding controls as follow... L.controlName().addTo(map);
-
-        // Control 1: This add the OpenStreetMap background tile
-        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/512/{z}/{x}/{y}?title=true&access_token={accessToken}', {
-            attribution: '',
-            maxZoom: 20,
-            accessToken: 'pk.eyJ1IjoicmVuaWVyLXRyZW51ZWxhIiwiYSI6ImNrZHhya2l3aTE3OG0ycnBpOWxlYjV3czUifQ.4hVvT7_fiVshoSa9P3uAew',
+        // Add the 'zoom' control
+        L.control.zoom({
+            position: 'topleft'
         }).addTo(map);
 
-
-        // Control 2: This add a scale to the map
+        // Add the 'scale' control
         L.control.scale().addTo(map);
+        L.control.locate({
+            position: "bottomright"
+        }).addTo(map);
 
-        // Control 3: This add a Search bar
-        var searchControl = new L.esri.Controls.Geosearch().addTo(map);
-
-        var results = new L.LayerGroup().addTo(map);
-
+        // Add the autocomplete text box
         var name = null, latlng = null, marker = null;
-        searchControl.on('results', function (data) {
-            results.clearLayers();
-            console.log(data);
-            for (var i = data.results.length - 1; i >= 0; i--) {
-                name = data.results[i].text;
-                latlng = data.results[i].latlng;
-                marker = L.marker(data.results[i].latlng, {
-                    draggable: true
-                });
-                results.addLayer(marker);
-            }
-            marker.on('dragend', function (e) {
-                latlng = marker.getLatLng();
-                console.log(latlng);
-            });
-        });
+        L.control.geocoder(key, {
+            placeholder: 'Search nearby...',
+            url: "https://api.locationiq.com/v1",
+            expanded: true,
+            panToPoint: true,
+            focus: true,
+            expanded: true,
+            markers: false,
+            params: {
+                countrycodes: 'PH'
+            },
+            position: "topleft"
+        }).on('select', function (ev) {
+            latlng = ev.latlng;
+            name = ev.feature.innerText
+            marker = L.marker(ev.latlng, {
+                draggable: true
+            }).addTo(map);
 
+            map.setZoom(16);
+
+            marker.on('dragend', function (event) {
+                var position = marker.getLatLng();
+                latlng = marker.getLatLng();
+                name = marker.getLatLng()
+            });
+        }).addTo(map);
 
         function confirmLocation() {
             $.ajax({
@@ -87,5 +102,7 @@
                 }
             });
         }
+
+        map.addControl(new L.Control.Permalink({useLocation: true, text: null}));
     </script>
 @endsection
