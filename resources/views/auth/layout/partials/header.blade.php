6e016affd267470016b81796fed59e37e1c3c672
Renier Trenuela <rtrenuela@objectbright.com>
2021-01-05 10:14:39 +0800

    update header design

--- resources/views/auth/layout/partials/header.blade.php
+++ resources/views/auth/layout/partials/header.blade.php
@@ -5,14 +5,14 @@
         </div>
     </button>
     <a class="c-header-brand d-lg-none" href="{{ route('p.index') }}">
-        <img src="{{ asset('img/header-logo.svg') }}" style="max-width: 150px; height: 30px">
+        <img src="{{ asset('img/broom-logo.png') }}" style="max-width: 150px; height: 30px">
     </a>
     <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true">
         <div class="c-icon c-icon-lg">
             <i class="fas fa-bars"></i>
         </div>
     </button>
-    <ul class="c-header-nav ml-auto mr-4">
+    <ul class="c-header-nav ml-auto mr-1">
         <li class="c-header-nav-item">
             <button id="btn-refresher" class="btn c-header-nav-link" onclick="reloader()">
                 <div class="c-icon"><i class="fas fa-sync-alt" style="margin-top: 6px;"></i></div>
@@ -25,7 +25,7 @@
             </script>
         </li>
         <li class="c-header-nav-item dropdown">
-            <a class="c-header-nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            <a class="c-header-nav-link p-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                 <div class="c-icon"><i class="fas fa-bell"></i></div>
             </a>
             <div class="dropdown-menu dropdown-menu-right pt-0">
@@ -43,7 +43,7 @@
         <li class="c-header-nav-item dropdown">
             <a class="c-header-nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                 <div class="c-icon" style="color: #2cea5f"><small><i class="fas fa-circle"></i></small></div>
-                <span class="mt-1">Hi, {{ auth()->user()->name }}</span>
+                <span class="mt-1"><small>Hi, {{ auth()->user()->name }}</small></span>
             </a>
             <div class="dropdown-menu dropdown-menu-right pt-0">
                 <div class="dropdown-header bg-light py-2"><strong>Account</strong></div>
@@ -60,13 +60,4 @@
             </div>
         </li>
     </ul>
-{{--    <div class="c-subheader px-3">--}}
-{{--        <!-- Breadcrumb-->--}}
-{{--        <ol class="breadcrumb border-0 m-0">--}}
-{{--            <li class="breadcrumb-item">Home</li>--}}
-{{--            <li class="breadcrumb-item"><a href="#">Admin</a></li>--}}
-{{--            <li class="breadcrumb-item active">Dashboard</li>--}}
-{{--            <!-- Breadcrumb Menu-->--}}
-{{--        </ol>--}}
-{{--    </div>--}}
 </header>
