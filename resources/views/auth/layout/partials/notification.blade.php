553be1c3448a3a022a631481b16746b27934f971
Renier Trenuela <rtrenuela@objectbright.com>
2021-02-09 15:21:18 +0800

    notif update

--- resources/views/auth/layout/partials/notification.blade.php
+++ resources/views/auth/layout/partials/notification.blade.php
@@ -1,15 +1,26 @@
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script>
-    @if(Session::has('success'))
-    swal("Success!", "{{ Session::get('success') }}", "success");
-    @endif
-    @if(Session::has('error'))
-    swal("Error!", "{{ Session::get('error') }}", "error");
-    @endif
-    @if(Session::has('warning'))
-    swal("Warning!", "{{ Session::get('warning') }}", "warning");
-    @endif
-    @if($errors->any())
-    swal("Error!", "@foreach($errors->all() as $item){{ $item.'\n' }}@endforeach", "error");
-    @endif
+        //window.history.forward(1);
+        console.log('hah')
+        console.log(document.referrer)
+        @if(Session::has('success'))
+        swal("Success!", "{{ Session::get('success') }}", "success").then(function(){
+            window.location = window.location.href;
+        });
+        @endif
+        @if(Session::has('error'))
+        swal("Error!", "{{ Session::get('error') }}", "error").then(function(){
+            window.location = window.location.href;
+        });
+        @endif
+        @if(Session::has('warning'))
+        swal("Warning!", "{{ Session::get('warning') }}", "warning").then(function(){
+            window.location = window.location.href;
+        });
+        @endif
+        @if($errors->any())
+        swal("Error!", "@foreach($errors->all() as $item){{ $item.'\n' }}@endforeach", "error").then(function(){
+            window.location = window.location.href;
+        });
+        @endif
 </script>
