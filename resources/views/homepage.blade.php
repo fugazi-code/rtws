fdee7ba1272d2b66acee8fb4cfca32abf903a4f8
Renier Trenuela <rtrenuela@objectbright.com>
2021-02-16 22:34:25 +0800

    change title

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,7 +9,7 @@
 <meta name="author" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-<title>BroomExpress</title>
+<title>{{ env('APP_NAME') }}</title>
 
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/animate.css">
